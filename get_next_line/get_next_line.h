e452bf81515f6e5d945201d1b41db597e55ee5aa
ap <ap@fedora>
2022-11-17 23:03:59 +0100

    get_next_line OK (125%)

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aprahin <marvin@42lausanne.ch>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 20:04:52 by aprahin           #+#    #+#             */
+/*   Updated: 2022/11/10 20:04:52 by aprahin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
+void	*ft_calloc(int count, int size);
+int		ft_strlen(const char *s);
+int		getnl(const char *s);
+char	*trimtonl(char **s);
+void	append(char **s, char **b);
+char	*endread(char **s, char *b, int r);
+int		makestr(char **s, int fd);
+char	*get_next_line(int fd);
+
+#endif
